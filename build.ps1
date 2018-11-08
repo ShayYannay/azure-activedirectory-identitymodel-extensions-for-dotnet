946af41a3a7c796408c2f79fce43dad5724ca002
George Krechar <dokrecar@microsoft.com>
2018-11-07 16:39:05 -0800

    Add GenerateContractAssemblies script
    * Script Generates Contract Assemblies, for each Target Framework, which will be
    used as reference assemblies during ApiCompat validation
    * Added -generateContractAssemblies flag to the build script as a way to
    invoke generateContractAssemblies flag

--- build.ps1
+++ build.ps1
@@ -5,7 +5,8 @@ param(
     [string]$root=$PSScriptRoot,
     [string]$runTests="YES",
     [string]$failBuildOnTest="YES",
-    [string]$slnFile="wilson.sln")
+    [string]$slnFile="wilson.sln",
+    [switch]$generateContractAssemblies)
 
 ################################################# Functions ############################################################
 
@@ -43,6 +44,12 @@ function CreateArtifactsRoot($folder)
     RemoveFolder($folder)
     Write-Host ">>> mkdir $folder | Out-Null"
     mkdir $folder | Out-Null
+}
+
+function GenerateContractAssemblies()
+{
+	# execute generateContractAssemblies script
+	& "$PSScriptRoot\generateContractAssemblies.ps1".
 }
 
 ################################################# Functions ############################################################
@@ -109,6 +116,13 @@ if($p.ExitCode -ne 0)
 	throw "Build failed."
 }
 popd
+
+if ($generateContractAssemblies.IsPresent)
+{
+	WriteSectionHeader("Generating Contract Assemblies");
+	GenerateContractAssemblies;
+	WriteSectionFooter("End Generating Contract Assemblies");
+}
 
 foreach($project in $buildConfiguration.SelectNodes("root/projects/src/project"))
 {
